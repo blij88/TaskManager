89964cd77c5b1e4c8143a05a9113b6643d05fc38
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2021-05-27 15:52:44 +0200

    Added methods that hopefully will let me write Contacts to db.

--- TaskManager.Data/Data/TaskDataRepository.cs
+++ TaskManager.Data/Data/TaskDataRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using TaskManager.Data.models;
 
 namespace TaskManager.Data
 {
@@ -12,36 +13,60 @@ namespace TaskManager.Data
             this.db = new TaskManagerDbContext();
         }
 
-        public void Add(models.Task task)
+        public void Add(Task task)
         {
             db.Tasks.Add(task);
             db.SaveChanges();
         }
-        public void AddContact(models.PeopleWhoCanHelp peopleWhoCanHelp)
-        {
-            db.PeopleWhoCanHelps.Add(peopleWhoCanHelp);
-            db.SaveChanges();
-        }
 
-        public void Delete(models.Task task)
+        public void Delete(Task task)
         {
             db.Tasks.Remove(task);
             db.SaveChanges();
         }
 
-        public models.Task Get(int id)
+        public Task Get(int id)
         {
             return db.Tasks.FirstOrDefault(t => t.Id == id);
         }
 
-        public List<models.Task> GetAll()
+        public List<Task> GetAll()
         {
             return db.Tasks.OrderBy(s => s.StartDate).ToList();
         }
 
-        public void Update(models.Task task)
+        public void Update(Task task)
         {
             var entry = db.Entry(task);
+            entry.State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public void AddContact(PeopleWhoCanHelp peopleWhoCanHelp)
+        {
+            db.PeopleWhoCanHelps.Add(peopleWhoCanHelp);
+            db.SaveChanges();
+        }
+
+        public void DeleteContact(PeopleWhoCanHelp peopleWhoCanHelp)
+        {
+            db.PeopleWhoCanHelps.Remove(peopleWhoCanHelp);
+            db.SaveChanges();
+        }
+
+        public PeopleWhoCanHelp GetContact(int id)
+        {
+            return db.PeopleWhoCanHelps.FirstOrDefault(p => p.Id == id);
+        }
+
+        public List<PeopleWhoCanHelp> GetAllContacts()
+        {
+            return db.PeopleWhoCanHelps.OrderBy(p => p.Name).ToList();
+        }
+
+        public void UpdateContact(PeopleWhoCanHelp peopleWhoCanHelp)
+        {
+            var entry = db.Entry(peopleWhoCanHelp);
             entry.State = EntityState.Modified;
             db.SaveChanges();
         }
